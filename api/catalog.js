854515a4d8ad77a68d1ce95275778a3b6ef29e6b
Mshxx <hgpbkz6kph@privaterelay.appleid.com>
2026-09-15 23:01:05 +0200

    Update catalog.js

--- api/catalog.js
+++ api/catalog.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     );
 
     url.searchParams.set("search", query);
-    url.searchParams.set("limit", "5");
+    url.searchParams.set("limit", "1");
 
     const response = await fetch(url, {
       headers: {
